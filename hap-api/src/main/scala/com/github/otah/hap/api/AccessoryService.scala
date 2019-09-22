b8cb45e9e2c6dd14f6316c81fc125156e4b20ca4
Ota Hauptmann <ota.hauptmann@gmail.com>
2019-09-22 18:42:28 +0200

    Add composition-way how to build a service

--- hap-api/src/main/scala/com/github/otah/hap/api/AccessoryService.scala
+++ hap-api/src/main/scala/com/github/otah/hap/api/AccessoryService.scala
@@ -1,7 +1,29 @@
 package com.github.otah.hap.api
 
+import com.github.otah.hap.api.characteristics.NameCharacteristic
+
 trait AccessoryService extends TypeConvenience {
+  parent =>
 
   def serviceType: HapType
   def characteristics: Seq[LowLevelCharacteristic]
+
+  def :+(characteristicToAdd: LowLevelCharacteristic): AccessoryService = new AccessoryService {
+
+    override def serviceType = parent.serviceType
+    override def characteristics = parent.characteristics :+ characteristicToAdd
+  }
+
+  def named(name: String) = this :+ NameCharacteristic(name)
+}
+
+object AccessoryService {
+
+  def apply(service: HapType): AccessoryService = new AccessoryService() {
+
+    override def serviceType = service
+    override def characteristics: Seq[LowLevelCharacteristic] = Nil
+  }
+
+  def apply(service: HapTypes.service.type => HapType): AccessoryService = apply(service(HapTypes.service))
 }
