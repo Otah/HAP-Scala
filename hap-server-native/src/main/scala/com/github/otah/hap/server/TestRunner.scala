700edb8c354e54bca8fc48b98f63ca0d02966644
Ota Hauptmann <ota.hauptmann@gmail.com>
2020-10-18 14:39:39 +0200

    Bridge the pair-setup phase to HAP Java as a library

--- hap-server-native/src/main/scala/com/github/otah/hap/server/TestRunner.scala
+++ hap-server-native/src/main/scala/com/github/otah/hap/server/TestRunner.scala
@@ -2,12 +2,14 @@ package com.github.otah.hap.server
 
 import java.net.InetAddress
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicReference
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.Unmarshal
+import io.github.hapjava.server.impl.pairing.SrpHandlerProxy
 
 import scala.concurrent.ExecutionContext
 import scala.util.Success
@@ -21,6 +23,9 @@ object TestRunner extends App {
   val md = "Blah1,1"
   val ci = 1
 
+  val pin = "999-00-666"
+  val salt = BigInt("-44368483166755255769016051680614042891")
+
   val adInfo = BonjourAccessoryAdvertiser.Info(mac, port, label, md, `c#`, ci, discoverable = true)
 
   val advertiser = new BonjourAccessoryAdvertiser(InetAddress.getByName("10.11.0.156"))
@@ -29,12 +34,24 @@ object TestRunner extends App {
   implicit val ec = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
   implicit val as = ActorSystem()
 
+  val srp = new AtomicReference[Option[SrpHandlerProxy]]()
+
   val handler = pathPrefix("pair-setup") {
     extractRequestEntity { entity =>
       complete {
         Unmarshal(entity).to[Array[Byte]] andThen {
           case Success(value) => println(s"Incoming: ${value.mkString(",")}")
-        } map (_ => StatusCodes.OK)
+        } map { incoming =>
+          if (TlvMessage(incoming).firstOfType(6).flatMap(_.value.headOption).contains(1)) srp.set(Some(SrpHandlerProxy(pin, salt)))
+
+          val responseBytes = srp.get().toArray.flatMap(_.responseFor(incoming))
+          println(s"Response: ${responseBytes.mkString(",")}")
+
+          HttpResponse(entity = HttpEntity(
+            ContentType(MediaType.applicationBinary("pairing+tlv8", MediaType.NotCompressible)),
+            responseBytes
+          ))
+        }
       }
     }
   } ~ extractRequest { request =>
